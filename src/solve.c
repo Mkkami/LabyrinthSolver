5cd493343b100456af8fd2122f3ee9488892cf0e
Michał Marciniak <michal.mar725@gmail.com>
2024-02-26 16:26:21 +0100

    Add function that checks if there's wall on the right

--- src/solve.c
+++ src/solve.c
@@ -4,21 +4,22 @@
 void print_steps(MoveInstruction *minst) {
     switch(minst->move) {
         case START:
-            printf("START\n");
+            printf("START");
             break;
         case FORWARD:
-            printf("FORWARD %d\n", minst->steps);
+            printf("FORWARD %d", minst->steps);
             break;
         case TURNRIGHT:
-            printf("TURNRIGHT %d\n", minst->steps);
+            printf("TURNRIGHT %d", minst->steps);
             break;
         case TURNLEFT:
-            printf("TURNLEFT %d\n", minst->steps);
+            printf("TURNLEFT %d", minst->steps);
             break;
         case STOP:
-            printf("STOP\n");
+            printf("STOP");
             break;
     }
+    printf("\n");
     minst->steps = 0;
 }
 
@@ -31,4 +32,25 @@ void init_direction(labyrinth *lab, MoveInstruction *minst, int height, int widt
         minst->dir = SOUTH;
     if (lab->pos_y == height)
         minst->dir = NORTH;
+}
+
+int check_right_x(labyrinth *lab, MoveInstruction *minst) {
+    int x = 2;
+    int y = 2;
+    
+    switch(minst->dir) {
+        case NORTH:
+            x++;
+            break;
+        case EAST:
+            y++;
+            break;
+        case SOUTH:
+            x--;
+            break;
+        case WEST:
+            y--;
+            break;
+    }
+    return lab->board[y][x] == 'X';
 }
