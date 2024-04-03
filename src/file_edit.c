91ec53fb20e2227001c173571bc0e6bb826ae738
Michał Marciniak <michal.mar725@gmail.com>
2024-04-03 21:53:09 +0200

    Code requirements changed

--- src/file_edit.c
+++
@@ -1,8 +0,0 @@
-#include "file_edit.h"
-
-void change_to_cell(int x, int y, FILE *in, const int width, const char element) {
-    int index =  ((y) * (width + 1)) + x;
-    fseek(in, index, SEEK_SET);
-    fputc(element, in);
-    // fseek(in, 0, SEEK_SET);
-}
